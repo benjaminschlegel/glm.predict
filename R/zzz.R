f4f0ed5aa480d0ca82f15853c4564da04668baa2
Benjamin E. Schlegel <kontakt@benjaminschlegel.ch>
2024-01-15 09:25:40 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("\nPlease cite as: \n")
   packageStartupMessage(" Schlegel, Benjamin E. (2024). glm.predict: Predicted Values and Discrete Changes for Regression Models.")
-  packageStartupMessage(" R package version 4.3-0. https://github.com/benjaminschlegel/glm.predict/ \n")
+  packageStartupMessage(" R package version 4.3-0. https://cran.r-project.org/package=glm.predict \n")
 }
